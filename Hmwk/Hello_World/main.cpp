df0e0b4fa07e44d1f0716b5d3f63fea3b8964430
dsnow75 <dps75@nau.edu>
2014-06-24 20:04:11 -0700

    Signed-off-by: dsnow75 <dps75@nau.edu>

---
+++ Hmwk/Hello_World/main.cpp
@@ -0,0 +1,23 @@
+/* 
+ * File:   main.cpp
+ * Author: David-Snow
+ *
+ * Created on June 23, 2014, 7:43 PM
+ */
+//System libraries
+#include <iostream>
+using namespace std;
+
+//user libraries
+
+//global constants
+
+//function prototypes
+
+//execution begins here
+
+int main(int argc, char** argv) {
+    //output simple text
+    cout<<"Hello World"<<endl;
+    return 0; 
+}
