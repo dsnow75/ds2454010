ccb8ad9761614df7c099ec3191f7cadb70e6cdc9
dsnow75 <dps75@nau.edu>
2014-07-13 22:14:43 -0700

    Signed-off-by: dsnow75 <dps75@nau.edu>

--- Projects/Project 1/main.cpp
+++ Projects/Project 1/main.cpp
@@ -19,9 +19,24 @@ using namespace std;
 int main(int argc, char** argv) {
     //variables
     int score; //score counter
-    int one, two, three, four; //numbers for game
-    int code1, code2, code3, code4; //code to guess
-    char redo; //retry variable
+    int guess; //number of guesses allowed
+    //numbers for game
+    int one = 1; 
+    int two = 2;
+    int three = 3;
+    int four = 4; 
+    //code to guess
+    int code1;
+    int code2;
+    int code3;
+    int code4; 
+    //guess variables
+    int guess1;
+    int guess2;
+    int guess3;
+    int guess4;
+    //retry variable
+    char redo; 
     do{
         srand(time(NULL));
         code1 = rand() % 4 + 1;
@@ -32,10 +47,21 @@ int main(int argc, char** argv) {
         cout << code2 << " ";
         cout << code3 << " ";
         cout << code4 << endl;
+        while (guess <= 10 && guess1 != code1 && guess2 != code2 && guess3 
+                != code3 && guess4 != code4){
+            //input guesses for code
+            cout << "Input the first number of the code you think it is: ";
+            cin >> guess1;
+            cout << "Input the second number of the code you think it is: ";
+            cin >> guess2;
+            cout << "Input the third number of the code you think it is: ";
+            cin >> guess3;
+            cout << "Input the fourth number of the code you think it is: ";
+            cin >> guess4;
+        }
         cout << "Retry? Y/N ";
         cin >> redo;
         
     }while (redo == 'y' || redo == 'Y');
     return 0;
 }
-
